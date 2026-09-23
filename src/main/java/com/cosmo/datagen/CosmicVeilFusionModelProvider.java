18d4f0ad69f0b7a79c40a375022b16e52f8980d6
CosmoLeveling <curtisjibo@gmail.com>
2026-09-23 18:21:18 -0500

    added a comment

--- src/main/java/com/cosmo/datagen/CosmicVeilFusionModelProvider.java
+++ src/main/java/com/cosmo/datagen/CosmicVeilFusionModelProvider.java
@@ -16,6 +16,8 @@ public class CosmicVeilFusionModelProvider extends FusionModelProvider {
 
     @Override
     protected void generate() {
+
+      // Generating Light Block Models for Fusion
         var modelData = ConnectingModelDataBuilder.builder()
                 .parent(new Identifier("block/cube_all"))
                 .texture("all", new Identifier(CosmicVeil.MOD_ID,"block/dark_shadow_rock_light"))
